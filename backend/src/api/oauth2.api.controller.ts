a433bf58ab710d689c298e480a636d16e512a534
Lucas Mösch <lmoesch@ukaachen.de>
2025-03-21 17:00:44 +0100

    Added dummy endpoints

--- backend/src/api/oauth2.api.controller.ts
+++ backend/src/api/oauth2.api.controller.ts
@@ -1,8 +1,15 @@
 import { Controller, Get, Request } from '@nestjs/common';
 
 @Controller('api/oauth2')
-export class TokenApiController {
+export class OAuth2ApiController {
   constructor() {}
 
+  @Get('authorize')
+  async authorize(@Request() req) {}
 
+  @Get('token')
+  async token(@Request() req) {}
+
+  @Get('certs')
+  async certs(@Request() req) {}
 }
