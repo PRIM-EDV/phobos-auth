14fcc8eb14fcd9bc12972f84d992581214ab532c
Lucas Mösch <lucasmoesch@live.de>
2025-04-20 16:49:10 +0200

    Fixed linking of mongodb database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './core/auth/auth.module';
 
 // Controllers
 import { AuthController } from './api/auth.controller';
 import { UserController } from './api/user.controller';
 
+const MONGO_DB_HOST = process.env.MONGO_DB_HOST ? process.env.MONGO_DB_HOST : 'localhost'
+
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(`mongodb://${MONGO_DB_HOST}/nest`),
   ],
   controllers: [
     AppController,
